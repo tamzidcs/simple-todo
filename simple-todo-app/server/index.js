02ccfa1d6a6583a763a5136aecb0cbd94a7b6a6d
Tamzid Chowdhury <7591587+tamzidcs@users.noreply.github.com>
2023-02-14 12:07:00 -0500

    whitespaces fixed

--- simple-todo-app/server/index.js
+++ simple-todo-app/server/index.js
@@ -3,11 +3,9 @@ const cors = require('cors')
 const fs = require('fs')
 let {pool: pool} = require("./src/app/db")
 const PORT = process.env.PORT || 3005;
-
 const app = express()
 app.use(express.json());
 app.use(cors());
-
 require('./src/app/routes/tasks.js')(app);
 
 app.listen(PORT, () => {
