c4f205776905309ee00b17bb0d2754d92f69a7be
Tamzid Chowdhury <7591587+tamzidcs@users.noreply.github.com>
2023-04-05 17:49:25 -0400

    dotenv updates

--- simple-todo-app/server/src/db.ts
+++ simple-todo-app/server/src/db.ts
@@ -1,17 +1,18 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
 import { Todo, TodoUser, User } from "./db/models";
 
 const { Sequelize } = require('sequelize-typescript');
 const config = require('./config/config');
 const sequelizeConf = {
-    database: "todolist_db",
-    user: "postgres",
-    password: "minat123",
-    host: 'localhost',
-    dialect: 'postgres',
+    database: process.env.DB_NAME,
+    user: process.env.DB_USERNAME,
+    password:  process.env.DB_PASSWORD,
+    host:  process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
     logging: false
 }
-
-
+debugger;
 const sequelize = new Sequelize(sequelizeConf.database,sequelizeConf.user,sequelizeConf.password , {
     host: sequelizeConf.host,
     dialect: sequelizeConf.dialect,
