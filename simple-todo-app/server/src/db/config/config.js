23f30b008ce9bdf438eddd398c2d0a313cfacf7e
Tamzid Chowdhury <7591587+tamzidcs@users.noreply.github.com>
2023-04-19 17:14:27 -0400

    db migration added

---
+++ simple-todo-app/server/src/db/config/config.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: "127.0.0.1",
+    dialect: "postgres",
+    port: 5432
+  },
+  test: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: "127.0.0.1",
+    dialect: "postgres",
+    port: 5432
+  },
+  production: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: "127.0.0.1",
+    dialect: "postgres",
+    port: 5432
+  }
+};
