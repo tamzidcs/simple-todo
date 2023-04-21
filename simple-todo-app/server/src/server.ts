ac205cf09e1ee9934808a660ac80ba15965dc84f
Tamzid Chowdhury <7591587+tamzidcs@users.noreply.github.com>
2023-04-20 20:30:25 -0400

    sync added back

--- simple-todo-app/server/src/server.ts
+++ simple-todo-app/server/src/server.ts
@@ -1,10 +1,10 @@
 import app from './app';
-import db from './db'; 
+import db from './db';
 
 const host = process.env.HOST || '127.0.0.1';
 const port = process.env.PORT || 3005;
 
 app.listen(port, async () => {
   console.log(`Server listening on http://${host}:${port}`);
-  await db.sync({ alter: true });
+   await db.sync({ alter: true });
 });
