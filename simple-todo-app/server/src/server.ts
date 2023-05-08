04f9ce4f6568fec3a462d2416dc1a74fc7515a7a
Tamzid Chowdhury <7591587+tamzidcs@users.noreply.github.com>
2023-05-08 12:46:19 -0400

    db setup script added

--- simple-todo-app/server/src/server.ts
+++ simple-todo-app/server/src/server.ts
@@ -1,10 +1,16 @@
 import app from './app';
 import db from './db'; 
 
-const host = process.env.HOST || '127.0.0.1';
+const host = process.env.HOST || "127.0.0.1";
 const port = process.env.PORT || 3005;
 
 app.listen(port, async () => {
   console.log(`Server listening on http://${host}:${port}`);
-  await db.sync({ alter: true });
+  try {
+    await db.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
 });
+
