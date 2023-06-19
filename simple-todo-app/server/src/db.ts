d884a5ba1a98d4ad5511cfb7f70f14b69902255d
Tamzid Chowdhury <7591587+tamzidcs@users.noreply.github.com>
2023-06-19 08:14:40 -0400

    code clean up

--- simple-todo-app/server/src/db.ts
+++ simple-todo-app/server/src/db.ts
@@ -28,7 +28,6 @@ sequelize.addModels([Todo, User, TodoUser]);
 export async function initializeDatabase() {
   try {
     await sequelize.authenticate();
-    console.log("Connection has been established successfully.");
   } catch (error) {
     console.error("Unable to connect to the database:", error);
   }
