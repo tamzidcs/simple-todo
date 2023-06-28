9b95366e343c8c6f2cdf05831eee4088a002ab61
Tamzid Chowdhury <7591587+tamzidcs@users.noreply.github.com>
2023-06-28 07:26:46 -0400

    test updates with real db

--- simple-todo-app/server/src/__tests__/todos.test.ts
+++ simple-todo-app/server/src/__tests__/todos.test.ts
@@ -3,37 +3,63 @@ import status from "http-status";
 import { describe, expect, test } from "@jest/globals";
 import { initializeDatabase } from "../db";
 import app from "../app";
-import { User } from "../db/models";
+import { Todo, TodoUser, User } from "../db/models";
+import { registerUser } from "../service/user.service";
+import { addNewTodo } from "../service/todo.service";
+import todo from "../interface/todo";
 
 const createUsers = async () => {
   const user = new User({
     username: "user1",
     password: "123456",
   });
-  try {
-    await user.save();
-  } catch (error) {
-    console.log(error);
-  }
 
   const user2 = new User({
     username: "user2",
     password: "123456",
   });
-  try {
-    await user2.save();
-  } catch (error) {
-    console.log(error);
-  }
+
+  await registerUser(user);
+  await registerUser(user2);
 };
 
-describe("Todo", () => {
-  beforeEach(async () => {
-    jest.clearAllMocks();
-    initializeDatabase();
-    createUsers();
-  });
+let todoId: Number;
+const createToDo = async () => {
+  const newTodo: todo = {
+    title: "todo1",
+    description: "description1",
+    username: "user1",
+    status: "pending",
+    id: "",
+  };
 
+  const newToDoResponse = await addNewTodo(newTodo);
+  todoId = newToDoResponse.id;
+};
+
+beforeAll(async () => {
+  jest.clearAllMocks();
+  initializeDatabase();
+  await createUsers();
+  await createToDo();
+});
+
+afterAll(async () => {
+  await User.destroy({
+    where: {},
+    truncate: true
+  });
+  await Todo.destroy({
+    where: {},
+    truncate: true
+  });
+  await TodoUser.destroy({
+    where: {},
+    truncate: true
+  });
+});
+
+describe("Todo", () => {
   describe("GET /todos", () => {
     it("should respond with a 200 status code", async () => {
       const response = await request(app).get("/todos/user1");
@@ -44,8 +70,8 @@ describe("Todo", () => {
   describe("POST /todos", () => {
     it("should respond with a 200 status code", async () => {
       const response = await request(app).post("/todos").send({
-        title: "todo1",
-        description: "description1",
+        title: "todo2",
+        description: "description2",
         status: "pending",
         username: "user1",
       });
@@ -56,7 +82,7 @@ describe("Todo", () => {
   describe("POST /share", () => {
     it("should respond with a 200 status code", async () => {
       const response = await request(app).post("/share").send({
-        todoId: 10,
+        todoId: todoId,
         username: "user2",
       });
       expect(response.status).toBe(status.OK);
@@ -65,7 +91,7 @@ describe("Todo", () => {
 
   describe("PATCH /todos", () => {
     it("should respond with a 200 status code", async () => {
-      const response = await request(app).patch("/todos/1");
+      const response = await request(app).patch("/todos/" + todoId);
       expect(response.status).toBe(status.OK);
     });
   });
--- simple-todo-app/server/src/__tests__/users.test.ts
+++ simple-todo-app/server/src/__tests__/users.test.ts
@@ -1,75 +1,66 @@
 import request from "supertest";
 import status from "http-status";
 import { describe, expect, test } from "@jest/globals";
-import db, { initializeDatabase } from "../db";
+import { initializeDatabase } from "../db";
 import app from "../app";
 import { User } from "../db/models";
+import { registerUser } from "../service/user.service";
 
 const createUser = async () => {
   const user = new User({
-    username: "user1",
-    password: "123456"
+    username: "user3",
+    password: "123456",
   });
-  try {
-    await user.save();
-  } catch (error) {
-    console.log(error);
-  }
+
+  await registerUser(user);
 };
 
-describe("Users",()=>{
-  beforeAll(async () => {
-    jest.clearAllMocks();
-    initializeDatabase();
-    createUser();
+beforeAll(async () => {
+  jest.clearAllMocks();
+  initializeDatabase();
+  await createUser();
+});
+
+afterAll(async () => {
+  await User.destroy({
+    where: {},
+    truncate: true
   });
-  
+});
+
+describe("Users", () => {
   describe("POST /users", () => {
     it("should return the JSON for the created user", async () => {
       const user = {
-        username: "user2",
+        username: "user4",
         password: "123456",
       };
       const response = await request(app).post("/users").send(user);
       expect(response.status).toEqual(status.OK);
     });
   });
-  
+
   describe("GET /users", () => {
     it("should respond with a 200 status code", async () => {
       const response = await request(app).get("/users");
       expect(response.status).toEqual(status.OK);
     });
+
+   
   });
-  
+
   describe("POST /login", () => {
     it("should respond with a 200 status code", async () => {
       const user = {
-        username: "user1",
+        username: "user3",
         password: "123456",
       };
       const response = await request(app).post("/login").send(user);
       expect(response.status).toEqual(status.OK);
       const userResponse = response.body;
       expect(userResponse).toEqual({
-        username: "user1",
+        username: "user3",
       });
     });
   });
-  
-  describe("POST /users", () => {
-    it("should respond with a 200 status code", async () => {
-      const user = {
-        username: "user1",
-        password: "123456",
-      };
-      const response = await request(app).post("/users").send(user);
-      expect(response.status).toEqual(status.OK);
-      const userResponse = response.body;
-      expect(userResponse).toEqual({
-        username: "user1",
-      });
-    });
-  });
-})
-
+});
