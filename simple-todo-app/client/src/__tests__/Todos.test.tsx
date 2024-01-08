6cdf906fdee6d475039c79a5fbb5d68baf62d4e5
Tamzid Chowdhury <7591587+tamzidcs@users.noreply.github.com>
2024-01-08 17:54:07 -0500

    todo-basic-jest-eslint-update

--- simple-todo-app/client/src/__tests__/Todos.test.tsx
+++ simple-todo-app/client/src/__tests__/Todos.test.tsx
@@ -1,11 +1,11 @@
 import { shallow } from 'enzyme';
 import React from 'react';
+import axios from 'axios';
+import { render, waitFor, screen } from '@testing-library/react';
 import ToDoList from '../components/Todo/ToDoList';
-import axios from "axios";
-import { render, waitFor, screen } from "@testing-library/react";
 
 const mockedUsedNavigate = jest.fn();
-jest.mock("axios");
+jest.mock('axios');
 jest.mock('react-router-dom', () => ({
   ...jest.requireActual('react-router-dom'),
   useNavigate: () => mockedUsedNavigate,
@@ -14,33 +14,32 @@ jest.mock('react-router-dom', () => ({
 const dummyTodos = [
   {
     id: 1,
-    title: "todo1",
-    description: "desc1",
-    status: "pending"
+    title: 'todo1',
+    description: 'desc1',
+    status: 'pending',
   },
   {
     id: 2,
-    title: "todo2",
-    description: "desc2",
-    status: "pending"
+    title: 'todo2',
+    description: 'desc2',
+    status: 'pending',
   },
   {
     id: 3,
-    title: "todo3",
-    description: "desc3",
-    status: "pending"
+    title: 'todo3',
+    description: 'desc3',
+    status: 'pending',
   },
 ];
 
-it("todos list", async () => {
+it('todos list', async () => {
   localStorage.setItem('username', 'user1');
   (axios.get as jest.Mock).mockResolvedValue({ data: dummyTodos });
   render(<ToDoList />);
-  const todoList = await waitFor(() => screen.findAllByTestId("todo"));
+  const todoList = await waitFor(() => screen.findAllByTestId('todo'));
   expect(todoList).toHaveLength(3);
 });
 
-it("should render signup", () => {
+it('should render signup', () => {
   const wrapper = shallow(<ToDoList />);
 });
-
