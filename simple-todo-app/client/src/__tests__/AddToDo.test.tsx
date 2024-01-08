e6ad9fd440291f43a29728831b2412cf07cca813
Tamzid Chowdhury <7591587+tamzidcs@users.noreply.github.com>
2024-01-08 18:13:20 -0500

    jest-fixes-eslint

--- simple-todo-app/client/src/__tests__/AddToDo.test.tsx
+++ simple-todo-app/client/src/__tests__/AddToDo.test.tsx
@@ -1,53 +1,55 @@
 import React from 'react';
 import AddToDo from '../components/AddTodo/AddTodo';
-import axios from "axios";
-import { render, waitFor, screen, fireEvent } from "@testing-library/react";
+import axios from 'axios';
+import {
+  render, waitFor, screen, fireEvent,
+} from '@testing-library/react';
 import { postTodo } from '../api/todos';
 import { todo } from '../interfaces/todo';
 
 const mockedUsedNavigate = jest.fn();
-jest.mock("axios");
+jest.mock('axios');
 jest.mock('react-router-dom', () => ({
   ...jest.requireActual('react-router-dom'),
   useNavigate: () => mockedUsedNavigate,
 }));
 
 const newTodo: todo = {
-  title: "todo1",
-  description: "desc1",
-  username: 'user1'
+  title: 'todo1',
+  description: 'desc1',
+  username: 'user1',
 };
 const mockTaskListUpdate = jest.fn();
 
-describe("AddToDo", () => {
+describe('AddToDo', () => {
   beforeEach(() => {
     localStorage.setItem('username', 'user1');
     render(<AddToDo taskListUpdate={mockTaskListUpdate} />);
   });
-  describe("when clicked", () => {
+  describe('when clicked', () => {
     let addButton: HTMLInputElement;
     let titleTextField: HTMLInputElement;
     let descriptionTextField: HTMLInputElement;
-    beforeEach( async() => {
-      addButton = screen.getByTestId("add-button") as HTMLInputElement;
-      titleTextField = screen.getByTestId("title-textfield") as HTMLInputElement;
-      descriptionTextField = screen.getByTestId("description-textfield") as HTMLInputElement;
-      const jsdomAlert = window.alert;  
-      window.alert = () => {};  
+    beforeEach(async () => {
+      addButton = screen.getByTestId('add-button') as HTMLInputElement;
+      titleTextField = screen.getByTestId('title-textfield') as HTMLInputElement;
+      descriptionTextField = screen.getByTestId('description-textfield') as HTMLInputElement;
+      const jsdomAlert = window.alert;
+      window.alert = () => {};
       (axios.post as jest.Mock).mockResolvedValue({ data: newTodo });
       const result = await postTodo(newTodo);
       await waitFor(() => fireEvent.change(titleTextField, { target: { value: 'title1' } }));
       await waitFor(() => fireEvent.change(descriptionTextField, { target: { value: 'description1' } }));
       await waitFor(() => fireEvent.click(addButton));
     });
-    it("title textfield has the correct value", async () => {
+    it('title textfield has the correct value', async () => {
       expect(titleTextField.value).toBe('title1')
-    })
-    it("description textfield has the correct value", async () => {
+    });
+    it('description textfield has the correct value', async () => {
       expect(descriptionTextField.value).toBe('description1')
-    })
-    it("calls the prop function to update list", async () => {
+    });
+    it('calls the prop function to update list', async () => {
       expect(mockTaskListUpdate.mock.calls).toHaveLength(1);
-    })
+    });
   });
 });
--- simple-todo-app/client/src/__tests__/ToDoList.test.tsx
+++ simple-todo-app/client/src/__tests__/ToDoList.test.tsx
@@ -1,11 +1,11 @@
 import { shallow } from 'enzyme';
 import React from 'react';
+import axios from 'axios';
+import { render, waitFor, screen } from '@testing-library/react';
 import ToDoList from '../components/Todo/ToDoList';
-import axios from "axios";
-import { render, waitFor, screen } from "@testing-library/react";
 
 const mockedUsedNavigate = jest.fn();
-jest.mock("axios");
+jest.mock('axios');
 jest.mock('react-router-dom', () => ({
   ...jest.requireActual('react-router-dom'),
   useNavigate: () => mockedUsedNavigate,
@@ -14,29 +14,29 @@ jest.mock('react-router-dom', () => ({
 const dummyTodos = [
   {
     id: 1,
-    title: "todo1",
-    description: "desc1",
-    status: "pending"
+    title: 'todo1',
+    description: 'desc1',
+    status: 'pending',
   },
   {
     id: 2,
-    title: "todo2",
-    description: "desc2",
-    status: "pending"
+    title: 'todo2',
+    description: 'desc2',
+    status: 'pending',
   },
   {
     id: 3,
-    title: "todo3",
-    description: "desc3",
-    status: "pending"
+    title: 'todo3',
+    description: 'desc3',
+    status: 'pending',
   },
 ];
-describe("ToDoList", () => {
-  it("should render todos list", async () => {
+describe('ToDoList', () => {
+  it('should render todos list', async () => {
     localStorage.setItem('username', 'user1');
     (axios.get as jest.Mock).mockResolvedValue({ data: dummyTodos });
     render(<ToDoList />);
-    const todoList = await waitFor(() => screen.findAllByTestId("todo"));
+    const todoList = await waitFor(() => screen.findAllByTestId('todo'));
     expect(todoList).toHaveLength(3);
   });
 });
