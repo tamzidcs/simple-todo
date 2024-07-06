1d574cfe469e59fdbda4535512b9c36eca4c2e50
Tamzid Chowdhury <7591587+tamzidcs@users.noreply.github.com>
2024-07-06 17:18:37 -0400

    lower case page view folder added

---
+++ simple-todo/client/src/components/pages/Landing/Landing.tsx
@@ -0,0 +1,7 @@
+import { Navigate } from 'react-router-dom';
+import React from 'react';
+
+function Landing() {
+  return <Navigate to="/login" />;
+}
+export default Landing;
---
+++ simple-todo/client/src/components/views/Header/Header.tsx
@@ -0,0 +1,30 @@
+import * as React from 'react';
+import { useNavigate } from 'react-router-dom';
+import './Header.scss';
+import Button from '../Button/Button';
+
+export function Header() {
+  const navigate = useNavigate();
+  const signout = () => {
+    localStorage.setItem('username', '');
+    navigate('/login');
+  };
+
+  return (
+    <div className="header-container">
+      <div className="username-container">
+        {localStorage.getItem('username')}
+      </div>
+      <div className="signout-button-container">
+        <Button
+          className="button signout-button"
+          testId="signout-button"
+          text="Signout"
+          type="button"
+          onClick={signout}
+        />
+      </div>
+    </div>
+  );
+}
+export default Header;
