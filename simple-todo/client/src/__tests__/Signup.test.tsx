f3fbc4f3491cfcf89d8e6ca7ad84359e5cfc8564
Tamzid Chowdhury <7591587+tamzidcs@users.noreply.github.com>
2025-03-03 18:37:58 -0500

    #ST-70 signup unit test form submission

--- simple-todo/client/src/__tests__/Signup.test.tsx
+++ simple-todo/client/src/__tests__/Signup.test.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import Signup from '../components/pages/Signup/Signup';
 
 const mockedUsedNavigate = jest.fn();
+const mockHandleOnSubmit = jest.fn();
 jest.mock('react-router-dom', () => ({
   ...jest.requireActual('react-router-dom'),
   useNavigate: () => mockedUsedNavigate,
@@ -15,5 +16,19 @@ describe('Signup', () => {
     expect(screen.getByLabelText('Username')).toBeInTheDocument();
     expect(screen.getByLabelText('Password')).toBeInTheDocument();
     expect(screen.getByRole('button', { name: 'Signup' })).toBeInTheDocument();
+  });
+
+  it('should submit the form with valid data', async () => {
+    render(<Signup />);
+    screen.getByRole('form', { name: 'signup-form' }).onsubmit = mockHandleOnSubmit;
+    fireEvent.change(screen.getByTestId('username-textfield'), {
+      target: { value: 'user1' },
+    });
+    fireEvent.change(screen.getByTestId('password-textfield'), {
+      target: { value: 'pass123' },
+    });
+    await fireEvent.click(screen.getByTestId('signup-button'));
+
+    expect(mockHandleOnSubmit).toHaveBeenCalled();
   });
 });
