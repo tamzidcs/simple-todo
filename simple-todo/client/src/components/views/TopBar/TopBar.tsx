43b34da00ed33285fd0eae75e3704ef616c73e6d
Tamzid Chowdhury <7591587+tamzidcs@users.noreply.github.com>
2025-08-26 18:59:34 -0400

    #ST-87 topbar logo added

--- simple-todo/client/src/components/views/TopBar/TopBar.tsx
+++ simple-todo/client/src/components/views/TopBar/TopBar.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { useNavigate } from 'react-router-dom';
 import './TopBar.scss';
 import Button from '../Button/Button';
+import simpleTodoTopbarLogo from '../../../resources/assets/simple-todo-topbar-logo.png';
 
 export function Header() {
   const navigate = useNavigate();
@@ -12,10 +13,15 @@ export function Header() {
 
   return (
     <div className="header-container">
-      <div className="username-container">
-        {localStorage.getItem('username')}
+      <div className="topbar-left">
+        <img src={simpleTodoTopbarLogo} alt="simple-todo-topbar-logo" />
       </div>
-      <div className="signout-button-container">
+      <div className="topbar-center"> </div>
+
+      <div className="topbar-right">
+        <div className="username-container">
+          {localStorage.getItem('username')}
+        </div>
         <Button
           className="button signout-button"
           testId="signout-button"
