7b03c8e6c8b0403c1c390a55cf28d87f8b357578
Tamzid Chowdhury <7591587+tamzidcs@users.noreply.github.com>
2026-01-03 18:31:21 -0500

    #ST-102 redux added with todolist state

--- simple-todo/client/src/App.tsx
+++ simple-todo/client/src/App.tsx
@@ -5,24 +5,28 @@ import {
   Route,
 } from 'react-router-dom';
 import React from 'react';
+import { Provider } from 'react-redux';
 import { TodoList } from './components/pages/TodoList/TodoList';
 import { Landing } from './components/pages/Landing/Landing';
 import { Signup } from './components/pages/Signup/Signup';
 import { Login } from './components/pages/Login/Login';
 import PrivateRoutes from './routes/PrivateRoutes';
+import { store } from './store/store';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route element={<PrivateRoutes />}>
-          <Route path="/toDoList" element={<TodoList />} />
-        </Route>
-        <Route path="/" element={<Landing />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Routes>
+          <Route element={<PrivateRoutes />}>
+            <Route path="/toDoList" element={<TodoList />} />
+          </Route>
+          <Route path="/" element={<Landing />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Router>
+    </Provider>
   );
 }
 export default App;
