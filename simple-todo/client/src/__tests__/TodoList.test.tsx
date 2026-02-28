e487615118b018603d3c891ee72daac776df5ab3
Tamzid Chowdhury <7591587+tamzidcs@users.noreply.github.com>
2026-02-28 16:09:13 -0500

    #ST-109 initial state from test todo

--- simple-todo/client/src/__tests__/TodoList.test.tsx
+++ simple-todo/client/src/__tests__/TodoList.test.tsx
@@ -11,21 +11,21 @@ jest.mock('react-router-dom', () => ({
   useNavigate: () => mockedUsedNavigate,
 }));
 
-const dummyTodos = [
+const testTodos = [
   {
-    id: 1,
+    id: '1',
     title: 'todo1',
     description: 'desc1',
     status: 'pending',
   },
   {
-    id: 2,
+    id: '2',
     title: 'todo2',
     description: 'desc2',
     status: 'pending',
   },
   {
-    id: 3,
+    id: '3',
     title: 'todo3',
     description: 'desc3',
     status: 'pending',
@@ -33,32 +33,13 @@ const dummyTodos = [
 ];
 
 const initialState = {
-  todos: [
-    {
-      id: '1',
-      title: 'todo1',
-      description: 'desc1',
-      status: 'pending',
-    },
-    {
-      id: '2',
-      title: 'todo2',
-      description: 'desc2',
-      status: 'pending',
-    },
-    {
-      id: '3',
-      title: 'todo3',
-      description: 'desc3',
-      status: 'pending',
-    },
-  ],
+  todos: testTodos,
 };
 
 describe('TodoList', () => {
   it('should render todos list', async () => {
     localStorage.setItem('username', 'user1');
-    (axios.get as jest.Mock).mockResolvedValue({ data: dummyTodos });
+    (axios.get as jest.Mock).mockResolvedValue({ data: testTodos });
     renderWithProvider(<TodoList />, { preloadedState: initialState });
     const todoList = await waitFor(() => screen.findAllByTestId('todo'));
     expect(todoList).toHaveLength(3);
