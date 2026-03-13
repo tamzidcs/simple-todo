e7b4084210eb14d9f4aeff54e2abe1109caf9512
Tamzid Chowdhury <7591587+tamzidcs@users.noreply.github.com>
2026-03-13 13:32:14 -0400

    #ST-116 migration to vite

---
+++ simple-todo/client/vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import viteTsconfigPaths from 'vite-tsconfig-paths';
+import svgr from 'vite-plugin-svgr';
+
+export default defineConfig({
+  plugins: [react(), viteTsconfigPaths(), svgr()],
+  server: {
+    open: true,
+    port: 3000,
+  },
+  build: {
+    outDir: 'build',
+  },
+});
