be0507c6fc0bacc768b1442f8f3e34ac7c822ef2
Tamzid Chowdhury <7591587+tamzidcs@users.noreply.github.com>
2026-04-27 18:20:17 -0400

    #ST-115 test erros fixed

--- simple-todo/client/src/setupTests.ts
+++ simple-todo/client/src/setupTests.ts
@@ -5,5 +5,18 @@
 import '@testing-library/jest-dom';
 import { configure } from 'enzyme';
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+import { vi } from "vitest";
 
 configure({ adapter: new Adapter() });
+
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: vi.fn().mockImplementation(query => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addEventListener: vi.fn(),
+    removeEventListener: vi.fn(),
+    dispatchEvent: vi.fn(),
+  })),
+});
