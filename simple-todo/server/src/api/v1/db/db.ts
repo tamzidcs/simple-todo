1b36bc33bb4a24bdc1bfaa23a56b7fc57df04bde
Tamzid Chowdhury <7591587+tamzidcs@users.noreply.github.com>
2026-08-09 16:40:22 -0400

    #ST-123 create db script added

--- simple-todo/server/src/api/v1/db/db.ts
+++ simple-todo/server/src/api/v1/db/db.ts
@@ -3,7 +3,6 @@ import dotenv from 'dotenv';
 import { AppDataSource } from "./data-source.js";
 dotenv.config({ path: `.env.${process.env.NODE_ENV}` })
 
-
 export async function initializeDatabase() {
   try {
     await AppDataSource.initialize();
