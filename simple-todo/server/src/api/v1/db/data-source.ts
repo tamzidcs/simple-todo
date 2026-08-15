44cd6855c7d70f420f3b9e7d0c759bf5993ac5a2
Tamzid Chowdhury <7591587+tamzidcs@users.noreply.github.com>
2026-08-14 20:53:51 -0400

    #ST-123 migration files added

--- simple-todo/server/src/api/v1/db/data-source.ts
+++ simple-todo/server/src/api/v1/db/data-source.ts
@@ -15,6 +15,6 @@ export const AppDataSource = new DataSource({
   synchronize: false,
   logging: false,
   entities: [User, Todo],
-  migrations: [],
+  migrations: [import.meta.dirname+"/migration/**/*.ts"],
   subscribers: [],
 });
