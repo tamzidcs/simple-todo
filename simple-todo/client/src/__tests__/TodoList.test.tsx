56520d5feffa2767597507a2f6ed168c4ff20851
Tamzid Chowdhury <7591587+tamzidcs@users.noreply.github.com>
2026-09-13 14:27:18 -0400

    #ST-133 todo done test updates

--- simple-todo/client/src/__tests__/TodoList.test.tsx
+++ simple-todo/client/src/__tests__/TodoList.test.tsx
@@ -1,10 +1,15 @@
 import axios from 'axios';
-import { waitFor, screen } from '@testing-library/react';
-import TodoList from '../components/pages/TodoList/TodoList';
+import { waitFor, screen, fireEvent } from '@testing-library/react';
+import TodoList from '../components/views/TodoList/TodoList';
 import renderWithProvider from '../utils/renderWithProvder';
 import { vi, type Mock } from 'vitest';
 
 const mockedUsedNavigate = vi.fn();
+const updateUserShareName = vi.fn();
+const handleShareTodo = vi.fn();
+const handleTodoDone = vi.fn();
+const handleDueDateChange = vi.fn();
+
 vi.mock('axios');
 vi.mock('react-router-dom', () => ({
   ...vi.importActual('react-router-dom'),
@@ -39,17 +44,42 @@ const testTodos = [
   },
 ];
 
+const userNameList = [{ id: '1', username: 'user1' }];
+
 const initialState = {
   todos: testTodos,
 };
 
-describe('TodoList', () => {
-  it('should render todos list', async () => {
+describe("TodoList", () => {
+   beforeEach(() => {
     localStorage.setItem('username', 'user1');
     (axios.get as Mock).mockResolvedValue({ data: testTodos });
-    // eslint-disable-next-line react/react-in-jsx-scope
-    renderWithProvider(<TodoList />, { preloadedState: initialState });
-    const todoList = await waitFor(() => screen.findAllByTestId('todo'));
+     renderWithProvider(
+      <TodoList
+        todoList={testTodos}
+        userNameList={userNameList}
+        updateUserShareName={updateUserShareName}
+        handleShareTodo={handleShareTodo}
+        handleTodoDone={handleTodoDone}
+        handleDueDateChange={handleDueDateChange}
+      />,
+      { preloadedState: initialState },
+    );
+  });
+  it("should render todos list", async () => {
+    const todoList = await waitFor(() => screen.findAllByTestId("todo"));
     expect(todoList).toHaveLength(3);
+  });
+  describe("TodoDone", () => {
+    describe("when done button is clicked", () => {
+      let doneButton: HTMLButtonElement;
+      it("should call todo done", async () => {
+        doneButton = screen.getByTestId(
+          "todo-done-button-1",
+        ) as HTMLButtonElement;
+        await waitFor(() => fireEvent.click(doneButton));
+        expect(handleTodoDone).toHaveBeenCalled();
+      });
+    });
   });
 });
